0c3fedd00ebd62f853303859f7ab801f473ae109
Quinn D. <danielss1@etsu.edu>
2025-04-14 00:43:06 -0400

    gameplan set
    created method gameplan for CRRUD functions with commented gameplan

---
+++ NetDexTest-01/NetDexTest-01/NetDexTest-01/Services/IPersonExt-ContactInfo.cs
@@ -0,0 +1,28 @@
+﻿namespace NetDexTest_01.Services
+{
+    // TODO: CONTACT INFO
+    public partial interface IPersonRepository // Contact-Info
+    {
+        // create just a contact info
+        // create a social media Contact
+
+        // create a person with ContactInfo, FullName, RecordCollector
+
+            // create a P & Ci,Fn,Rc using Authorixation
+
+        // read just a contact info
+        // read just a social media contact
+
+        // read all contacts
+
+        // read all contacts by person and username
+
+        // read all persons by person and userid
+
+        
+        // update/edit
+
+
+        // delete a social media contact
+    }
+}
