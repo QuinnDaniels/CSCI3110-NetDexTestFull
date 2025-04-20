78d57eae0009b521dd013b6529cde4f35e09bf85
Quinn D. <danielss1@etsu.edu>
2025-04-20 17:08:46 -0400

    more splitting

--- NetDexTest-01/NetDexTest-01/NetDexTest-01/Controllers/AuthApiController.cs
+++ NetDexTest-01/NetDexTest-01/NetDexTest-01/Controllers/AuthApiController.cs
@@ -15,7 +15,7 @@ namespace NetDexTest_01.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class AuthController : ControllerBase
+    public partial class AuthController : ControllerBase
     {
         //public static List<User> tempUserDb = new List<User>{
         //    new User{UserId="abc123",UserName="John", DisplayName="BilboBaggins", Email="john@abc.com", Password="john@123" },
@@ -170,102 +170,6 @@ namespace NetDexTest_01.Controllers
 
 
 
-        private async Task<ApplicationUser> AddUser(RegisterModel registerModel)
-        {
-            var newUser = await _userRepo.CreateUserDexHolderAsync(registerModel);
-            return newUser;
-        }
-        private async Task<IdentityResult> AddUser(RegisterModel registerModel, bool tf)
-        {
-            var result = await _userRepo.CreateUserDexHolderAsync(registerModel, tf);
-            return (IdentityResult)result;
-        }
-        private async Task<ApplicationUser?> GetUserByEmail(string email)
-        {
-            return await _userRepo.GetUserAsync(PropertyField.email, email);
-        }
-        /*-------------------------------*/
-
-        private async Task<ApplicationUser?> GetUser(string email, string password)
-        {
-            return await _userRepo.GetUserAsync(PropertyField.email, email, password);
-        }
-
-
-        private IActionResult BadRequestErrorMessages()
-        {
-            var errMsgs = ModelState.Values.SelectMany(v => v.Errors.Select(e => e.ErrorMessage));
-            return BadRequest(errMsgs);
-        }
-
-
-
-
-        private async Task<AuthenticatedResponse> GetTokens(ApplicationUser user)
-        {
-            //create claims details based on the user information
-            var claims = new[] {
-                        new Claim(JwtRegisteredClaimNames.Sub, _configuration["JwtSettings:Subject"]),
-                        new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                        new Claim(JwtRegisteredClaimNames.Iat, DateTime.UtcNow.ToString()),
-                        new Claim("UserId", user.Id),
-                        new Claim("UserName", user.UserName),
-                        new Claim("Email", user.Email)
-                    };
-
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"]));
-            var signIn = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
-            var token = new JwtSecurityToken(
-                _configuration["JwtSettings:Issuer"],
-                _configuration["JwtSettings:Audience"],
-                claims,
-                expires: DateTime.UtcNow.AddMinutes(Convert.ToDouble(_configuration["JwtSettings:MinutesToExpiration"])),
-                signingCredentials: signIn);
-            var tokenStr = new JwtSecurityTokenHandler().WriteToken(token);
-
-            var refreshTokenStr = GetRefreshToken();
-            user.RefreshToken = refreshTokenStr;
-            var authResponse = new AuthResponse { Token = tokenStr, RefreshToken = refreshTokenStr };
-            return await Task.FromResult(authResponse);
-        }
-        /*------------------------*/
-        private string GetRefreshToken()
-        {
-            var token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64));
-            // ensure token is unique by checking against db
-            var tokenIsUnique = !_userManager.Users.Any(u => u.RefreshToken == token);
-            //var user = _userManager.Users.FirstOrDefault(u => u.RefreshToken == request.RefreshToken);
-
-
-
-            if (!tokenIsUnique)
-                return GetRefreshToken();  //recursive call
-
-            return token;
-        }
-        private async Task<ApplicationUser> GetUserByRefreshToken(string refreshToken)
-        {
-            return await Task.FromResult(_userManager.Users.FirstOrDefault(u => u.RefreshToken == refreshToken));
-            //return await Task.FromResult(tempUserDb.FirstOrDefault(u => u.RefreshToken == refreshToken));
-        }
-
-        public ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
-        {
-            var tokenValidationParameters = new TokenValidationParameters
-            {
-                ValidateAudience = false, //you might want to validate the audience and issuer depending on your use case
-                ValidateIssuer = false,
-                ValidateIssuerSigningKey = true,
-                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["JwtSettings:Key"])),
-                ValidateLifetime = false //we don't care about the token's expiration date
-            };
-            var tokenHandler = new JwtSecurityTokenHandler();
-            var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out SecurityToken securityToken);
-            var jwtSecurityToken = securityToken as JwtSecurityToken;
-            if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
-                throw new SecurityTokenException("Invalid token");
-            return principal;
-        }
 
 
 
--- NetDexTest-01/NetDexTest-01/NetDexTest-01/Services/DbUserRepository.Delete.cs
+++ NetDexTest-01/NetDexTest-01/NetDexTest-01/Services/DbUserRepository.Delete.cs
@@ -7,6 +7,70 @@ namespace NetDexTest_01.Services
     // assisted partially with chatGPT
     public partial class DbUserRepository : IUserRepository
     {
+        public async Task DeleteAsync(PropertyField pType, string input)
+        {
+            switch (pType)
+            {
+                case PropertyField.id:
+                    await DeleteByIdAsync(input);
+                    break;
+                case PropertyField.username:
+                    await DeleteByUsernameAsync(input);
+                    break;
+                default:
+                    throw new InvalidOperationException("PropertyField is invalid! Check the switch-case and try again");
+
+            }
+        }
+
+        public async Task DeleteByIdAsync(string id)
+        {
+            var dexHolderToDelete = await ReadDexByIdAsync(id);
+            var userToDelete = await ReadByIdAsync(id);
+            if (dexHolderToDelete != null)
+            {
+                _db.DexHolder.Remove(dexHolderToDelete);
+                await _db.SaveChangesAsync();
+            }
+            if (userToDelete != null)
+            {
+                _db.Users.Remove(userToDelete);
+                await _db.SaveChangesAsync();
+
+            }
+        }
+        public async Task DeleteByUsernameAsync(string username)
+        {
+            var dexHolderToDelete = await ReadDexByUsernameAsync(username);
+            var userToDelete = await ReadByUsernameAsync(username);
+
+            try
+            {
+                if (userToDelete != null)
+                {
+                    _db.Users.Remove(userToDelete);
+                    await _db.SaveChangesAsync();
+
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex}");
+            }
+
+            try
+            {
+                if (dexHolderToDelete != null)
+                {
+                    _db.DexHolder.Remove(dexHolderToDelete);
+                    await _db.SaveChangesAsync();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"ERROR: {ex}");
+            }
+        }
 
     }
 }
